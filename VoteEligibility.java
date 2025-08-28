dbd3b8dea08f09b3b6d09a6527ee74db5f1c5902
Hemavarni1501 <hemavarni1501@gmail.com>
2025-08-28 06:34:24 +0000

    Add VoteEligibility Java Program

---
+++ VoteEligibility.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class VoteEligibility{
+    public static void main(String args[]){
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Voter Age Eligibility Checking Program");
+        System.out.println("Enter Age to Check Voting Criteria:");
+        int age= obj.nextInt();
+        if(age>18&&age<150){
+            System.out.println("Eligible for Voting \n Age="+age);
+        }
+        else{
+            System.out.println("Not Eligible for Voting \n Age="+age);
+        }
+        System.out.println("Thankyou!");
+    }
+}
