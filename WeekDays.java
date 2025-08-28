59b5cda9874c9742fa0c5b5fedfa8ea36bdd6acc
Hemavarni1501 <hemavarni1501@gmail.com>
2025-08-28 06:37:59 +0000

    Add WeekDays Java Program

---
+++ WeekDays.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class WeekDays{
+    public static void main(String args[]){
+        Scanner obj = new Scanner(System.in);
+        System.out.println("WeekDays SwitchCase");
+        System.out.println("Enter Number to Print the Day:");
+        int num= obj.nextInt();
+        switch (num){
+            case 1:{
+                System.out.println("Day:1\nSunday");
+                break;
+            }
+            case 2:{
+                System.out.println("Day:2\nMonday");
+                break;
+            }
+            case 3:{
+                System.out.println("Day:3\nTuesday");
+                break;
+            }
+            case 4:{
+                System.out.println("Day:4\nWednesday");
+                break;
+            }
+            case 5:{
+                System.out.println("Day:5\nThursday");
+                break;
+            }
+            case 6:{
+                System.out.println("Day:6\nFriday");
+                break;
+            }
+            case 7:{
+                System.out.println("Day:7\nSaturday");
+                break;
+            }
+            default:{
+                System.out.println("Invalid Input");
+            }
+        }
+        System.out.println("Thankyou!");
+    }
+}
