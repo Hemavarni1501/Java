ba7b84b1fa65c259f1bff8ec62bb8ad7634822dc
Hemavarni1501 <hemavarni1501@gmail.com>
2025-08-28 06:18:47 +0000

    Add Arithmatic JAva Program

---
+++ Arithmatic.java
@@ -0,0 +1,12 @@
+class Arithmatic {
+    public static void main(String[] args) {
+        int a=50, b=10;
+        System.out.println("ARITHMATIC OPERARIONS");
+        System.out.println("a="+a+" b="+b);
+        System.out.println("Add="+(a+b));
+        System.out.println("Sub="+(a-b));
+        System.out.println("Mul="+(a*b));
+        System.out.println("Div="+(a/b));
+        System.out.println("Mod="+(a%b));
+    }
+}
