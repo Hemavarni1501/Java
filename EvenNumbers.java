98268d275d04affb3e3f4a38ea5ada42b58f767a
Hemavarni1501 <hemavarni1501@gmail.com>
2025-08-28 06:27:03 +0000

    Add EvenNumbers Java Program

---
+++ EvenNumbers.java
@@ -0,0 +1,11 @@
+class EvenNumbers{
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println("First " + n + " even numbers:");
+        for (int i = 1; i <= n; i++) {
+            if(i%2==0){
+            System.out.println(i);
+            }
+        }
+    }
+}
