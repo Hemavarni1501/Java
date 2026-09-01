7e1bb1013565d15948405e0c2f787a6438f72939
Hemavarni Sivakumar <hemavarni1501@gmail.com>
2026-09-01 11:56:25 +0530

    Time: 2 ms (99.84%) | Memory: 45.9 MB (98.75%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,27 +1,24 @@
 class Solution {
-    public String convert(String s, int n) {
-        if(n==1||n>s.length()){
+    public String convert(String s, int numRows) {
+        if (numRows == 1 || numRows >= s.length()) {
             return s;
         }
-        String[] a=new String[n];
-        for(int i=0;i<n;i++){
-            a[i]="";
-        }
-        int l=2*n-2;
-        for(int i=0;i<s.length();i++){
-            int p=i%l;
-            int row;
-            if(p<n){
-                row=p;
-            }else{
-                row=l-p;
+
+        int n = s.length();
+        char[] result = new char[n];
+        int cycleLen = 2 * numRows - 2;
+        int idx = 0;
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j + i < n; j += cycleLen) {
+                result[idx++] = s.charAt(j + i);
+                
+                if (i != 0 && i != numRows - 1 && j + cycleLen - i < n) {
+                    result[idx++] = s.charAt(j + cycleLen - i);
+                }
             }
-            a[row]+=s.charAt(i);
         }
-        String result="";
-        for(int i=0;i<n;i++){
-            result+=a[i];
-        }
-        return result;
+
+        return new String(result);
     }
 }
