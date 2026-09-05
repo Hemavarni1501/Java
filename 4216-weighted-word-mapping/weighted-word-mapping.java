178e1cf14220047e690cf76cad4214637c43785b
Hemavarni Sivakumar <hemavarni1501@gmail.com>
2026-09-05 15:32:08 +0530

    Time: 2 ms (95.05%) | Memory: 46.6 MB (51.64%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        StringBuilder a=new StringBuilder();
+        for(int i=0;i<words.length;i++){
+            int s=0;
+            for(int j=0;j<words[i].length();j++){
+                int x=words[i].charAt(j)-'a';
+                s+=weights[x];
+            }
+            a.append((char) ('z' - (s % 26)));
+        }
+        return new String(a);
+    }
+}
