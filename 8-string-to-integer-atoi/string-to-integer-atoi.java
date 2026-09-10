2cd5a051ae84c56a0c223ded163f6b6338fd5acb
Hemavarni Sivakumar <hemavarni1501@gmail.com>
2026-09-10 14:05:22 +0530

    Time: 4 ms (2.85%) | Memory: 44.9 MB (9.69%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,33 +1,40 @@
 class Solution {
     public int myAtoi(String s) {
-        int i = 0, n = s.length();
-        
-        while (i < n && s.charAt(i) == ' ') {
-            i++;
-        }
-        
-        if (i == n) {
-            return 0;
-        }
-        
-        int sign = 1;
-        if (s.charAt(i) == '+' || s.charAt(i) == '-') {
-            sign = (s.charAt(i) == '-') ? -1 : 1;
-            i++;
-        }
-        
-        long ans = 0;
-        while (i < n && Character.isDigit(s.charAt(i))) {
-            ans = ans * 10 + (s.charAt(i) - '0');
-            if (sign == 1 && ans >= Integer.MAX_VALUE) {
-                return Integer.MAX_VALUE;
+        int j = 0;
+        int m = 1;
+        String t = s.trim();
+        if (t.length() > 0) {
+            String ans = "";
+            if (t.charAt(0) == '-' || t.charAt(0) == '+') {
+                m = t.charAt(0) == '-' ? -1 : 1;
             }
-            if (sign == -1 && -ans <= Integer.MIN_VALUE) {
-                return Integer.MIN_VALUE;
+            while (j < t.length()) {
+                if (!Character.isDigit(t.charAt(j))) {
+                    if (j == 0 && (t.charAt(j) == '-' || t.charAt(j) == '+')) {
+                        j++;
+                        continue;
+                    }
+                    break;
+                } else {
+                    ans += t.charAt(j++);
+                }
             }
-            i++;
+            long a = 0;
+            for (int i = 0; i < ans.length(); i++) {
+                int d = ans.charAt(i) - '0';
+                a = (a * 10) + d;
+                if (m == 1 && a >= Integer.MAX_VALUE) {
+                    return Integer.MAX_VALUE;
+                }
+                if (m == -1 && -a <= Integer.MIN_VALUE) {
+                    return Integer.MIN_VALUE;
+                }
+            }
+            if (m == -1) {
+                a = -a;
+            }
+            return (int) a;
         }
-        
-        return (int) (ans * sign);
+        return 0;
     }
 }
