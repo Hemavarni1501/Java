402d807c6adaae5fe9170157178b85a0bc18c4bd
Hemavarni Sivakumar <hemavarni1501@gmail.com>
2026-09-10 13:24:57 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (74.47%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,30 +1,33 @@
 class Solution {
     public int myAtoi(String s) {
-     s = s.trim();
-
-    if (s.isEmpty()) {
-        return 0;
-    }
-        int sign = 1;
-        long ans = 0;
-        int index = 0;
-        if (s.charAt(index) == '-' || s.charAt(index) == '+') {
-            if (s.charAt(index) == '-'){
-                sign = -1;
-            }
-            index++;
+        int i = 0, n = s.length();
+        
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
         }
-        while (index < s.length() && Character.isDigit(s.charAt(index))) {
-            ans = ans * 10 + (s.charAt(index) - '0');
-
-            if (sign == 1 && ans > Integer.MAX_VALUE) {
+        
+        if (i == n) {
+            return 0;
+        }
+        
+        int sign = 1;
+        if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+            sign = (s.charAt(i) == '-') ? -1 : 1;
+            i++;
+        }
+        
+        long ans = 0;
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            ans = ans * 10 + (s.charAt(i) - '0');
+            if (sign == 1 && ans >= Integer.MAX_VALUE) {
                 return Integer.MAX_VALUE;
             }
-            if (sign == -1 && -ans < Integer.MIN_VALUE) {
+            if (sign == -1 && -ans <= Integer.MIN_VALUE) {
                 return Integer.MIN_VALUE;
             }
-            index++;
+            i++;
         }
-        return (int) (sign* ans);  
+        
+        return (int) (ans * sign);
     }
 }
