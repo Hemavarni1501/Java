dfaa34dcfc1f67a0a9eb09df18177ef7c7b9a537
Hemavarni Sivakumar <hemavarni1501@gmail.com>
2026-09-22 10:06:56 +0530

    Time: 1 ms (100.00%) | Memory: 46.7 MB (16.17%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,15 +1,51 @@
-class Solution {
-    public int[] minOperations(String b) {
-        int n=b.length();
-        int[] res=new int[n];
-        char[] a=b.toCharArray();
-        for(int i=0;i<n;i++){
-            if(a[i]=='1'){
-            for(int j=0;j<n;j++){
-                res[j]+=Math.abs(j-i);
-            }
+class Solution1 {
+    public int[] minOperations(String boxes) {
+        int n = boxes.length();
+
+        int[] res = new int[n];
+
+        int balls = 0, moves = 0;
+
+        for (int i = 0; i < n; i++) {
+            res[i] += moves += balls;
+            if (boxes.charAt(i) == '1') balls++;
         }
+
+        balls = moves = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] += moves += balls;
+            if (boxes.charAt(i) == '1') balls++;
         }
+
         return res;
+    }
+}
+
+class Solution {
+    public int[] minOperations(String boxes) {
+        int n = boxes.length();
+        int[] res = new int[n];
+
+        solve(0, boxes, res, 0, 0);
+
+        return res;
+    }
+
+    private int[] solve(int i, String boxes, int[] res,
+                        int leftBalls, int leftMoves) {
+
+        if (i == boxes.length())
+            return new int[]{0, 0}; // {rightBalls, rightMoves}
+
+        res[i] += leftMoves += leftBalls;
+        if (boxes.charAt(i) == '1') leftBalls++;
+
+        int[] right = solve(i + 1, boxes, res, leftBalls, leftMoves);
+
+        res[i] += right[1] += right[0];
+        if (boxes.charAt(i) == '1') right[0]++;
+
+        return right;
     }
 }
