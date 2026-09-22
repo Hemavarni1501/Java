9b7e44bc772b01a3b37a30314722037339094305
Hemavarni Sivakumar <hemavarni1501@gmail.com>
2026-09-22 09:39:55 +0530

    Time: 3 ms (16.11%) | Memory: 43.1 MB (10.75%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String defangIPaddr(String a) {
+        String s="";
+        for(int i=0;i<a.length();i++){
+            if(a.charAt(i)=='.'){
+                s+='[';
+                s+=a.charAt(i);
+                s+=']';
+            }else{
+                s+=a.charAt(i);
+            }
+        }
+        return s;
+    }
+}
